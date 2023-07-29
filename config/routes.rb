24193e33fbfe50d90d6a9a8a0d3889cebf3f3135
Josh Goldfarb <36429498+The-Black-Fox@users.noreply.github.com>
2023-07-29 15:59:11 -0300

    Finished Chapter 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'movies#index'
-  get '/movies' => 'movies#index'
+  root "movies#index"
+  get "movies" => "movies#index"
+  get "movies/:id" => "movies#show"
+
+
 end
