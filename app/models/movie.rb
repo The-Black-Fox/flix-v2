e30f7c129036a565df7b0c1588690907b3207b60
Josh Goldfarb <36429498+The-Black-Fox@users.noreply.github.com>
2023-09-07 20:48:53 -0300

    Chapter 38 A

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
+  has_many :characterizations, dependent: :destroy
+  has_many :genres, through: :characterizations
   has_many :critics, through: :reviews, source: :user
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
