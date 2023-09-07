01d4227352897fe9e467f103f5c3d80b64c79902
Josh Goldfarb <36429498+The-Black-Fox@users.noreply.github.com>
2023-09-07 13:21:17 -0300

    Completed Chapter 37 A

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@ module UsersHelper
 
   def profile_image(user, size=80)
     gravatar_url = "http://secure.gravatar.com/avatar/#{user.gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: "#{user.name} Gravatar" )
+    image_tag(gravatar_url, alt: "#{user.name} Gravatar", size: size)
   end
 end
