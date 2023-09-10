141cbc700bd9373055f7a63db3a047e59182e33a
Josh Goldfarb <36429498+The-Black-Fox@users.noreply.github.com>
2023-09-09 22:39:41 -0300

    Completed Chapter 42

---
+++ db/migrate/20230909180929_remove_image_file_attribute_from_movies.rb
@@ -0,0 +1,5 @@
+class RemoveImageFileAttributeFromMovies < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :movies, :image_file_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_08_231540) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_09_180929) do
   create_table "characterizations", force: :cascade do |t|
     t.integer "movie_id", null: false
     t.integer "genre_id", null: false
@@ -45,7 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_231540) do
     t.date "released_on"
     t.string "director"
     t.string "duration"
-    t.string "image_file_name", default: "placeholder.png"
     t.string "slug"
   end
 
